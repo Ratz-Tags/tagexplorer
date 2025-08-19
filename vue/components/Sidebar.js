81995f51031d15a0ca93ac382f6de12643781b48
NotRatz <bmcnaughton0@gmail.com>
2025-08-19 03:13:58 -0500

    refactor: remove sidebar template and use render function

--- vue/components/Sidebar.js
+++ vue/components/Sidebar.js
@@ -5,13 +5,7 @@ export default {
   mounted() {
     initSidebar();
   },
-  template: `
-    <aside class="sidebar-wrapper" aria-label="Copied artists">
-      <div id="copied-sidebar" class="sidebar-hidden" role="complementary" aria-label="Artists you've copied">
-        <button class="copied-sidebar-close" aria-label="Close sidebar">×</button>
-        <h2 class="visually-hidden">Copied Artists</h2>
-        <!-- Copied artists will be injected here -->
-      </div>
-    </aside>
-  `
+  render() {
+    return null;
+  }
 };
