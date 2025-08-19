7a8d45585812903dd084c7af6f001aedc039786d
Rat <bmcnaughton0@gmail.com>
2025-08-19 09:53:15 -0500

    feat: add categorized collapsible tag toolbar

--- main.js
+++ main.js
@@ -1,3 +1,22 @@
 import App from './src/App.js';
+import { loadAppData } from './modules/api.js';
+import { setTagTooltips, setTagTaunts, setTaunts } from './modules/tags.js';
+import { setAllArtists as setGalleryArtists, filterArtists } from './modules/gallery.js';
+import { setAllArtists as setExplorerArtists } from './modules/tag-explorer.js';
+import { setAllArtists as setSidebarArtists } from './modules/sidebar.js';
 const { createApp } = Vue;
-createApp(App).mount('#app');
+
+loadAppData()
+  .then(({ artists, tooltips, generalTaunts, tagTaunts }) => {
+    setGalleryArtists(artists);
+    setExplorerArtists(artists);
+    setSidebarArtists(artists);
+    setTagTooltips(tooltips);
+    setTagTaunts(tagTaunts);
+    setTaunts(generalTaunts);
+    createApp(App).mount('#app');
+    filterArtists();
+  })
+  .catch(() => {
+    createApp(App).mount('#app');
+  });
