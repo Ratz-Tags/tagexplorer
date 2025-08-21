44e3bb619b9195d709b8f5c29eac2ab478083820
NotRatz <bmcnaughton0@gmail.com>
2025-08-21 05:36:49 -0500

    fix: correct formatting of Azure TTS key assignment

--- azure-tts.local.js
+++ azure-tts.local.js
@@ -1,3 +1,2 @@
-window._azureTTSKey =
-  "4j6HXsUkTc0l9IpTaFPnBJc5Sgi7qLqqUz5lbo1zpii4ddmPkRMtJQQJ99BGAC1i4TkXJ3w3AAAYACOGOBrd";
+window._azureTTSKey = "4j6HXsUkTc0l9IpTaFPnBJc5Sgi7qLqqUz5lbo1zpii4ddmPkRMtJQQJ99BGAC1i4TkXJ3w3AAAYACOGOBrd";
 window._azureTTSRegion = "centralus";
